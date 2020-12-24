82c0cbffe0911e85c0e4e81dd348755414bbb6de
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-12-24 16:59:31 -0300

    ✅ Add Tests for Authentication

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,19 @@ Route::group([
     'prefix' => 'auth'
 
 ], function ($router) {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+    Route::domain('auth')
+        ->name('auth.register')
+        ->post('/register', [AuthController::class, 'register']);
+    Route::domain('auth')
+        ->name('auth.login')
+        ->post('/login', [AuthController::class, 'login']);
+    Route::domain('auth')
+        ->name('auth.profile')
+        ->get('/user-profile', [AuthController::class, 'userProfile']);
+    Route::domain('auth')
+        ->name('auth.refresh')
+        ->post('/refresh', [AuthController::class, 'refresh']);
+    Route::domain('auth')
+        ->name('auth.logout')
+        ->post('/logout', [AuthController::class, 'logout']);
 });
---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\TestResponse;
+use Tests\TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    private function generateANewUser()
+    {
+        return User::factory()->make([
+            'password' => $this->faker->password
+        ]);
+    }
+
+    private function createANewUser(User $user): TestResponse
+    {
+        return $this->post(route('auth.register'), [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => $user->password,
+            'password_confirmation' => $user->password
+        ]);
+    }
+
+    private function login(User $user): TestResponse
+    {
+        return $this->post(route('auth.login'), [
+            'email' => $user->email,
+            'password' => $user->password,
+        ]);
+    }
+
+    public function testRegisterRoute()
+    {
+        $user = $this->generateANewUser();
+        $response = $this->createANewUser($user);
+        $response->assertSuccessful()
+            ->assertStatus(201)
+            ->assertJsonStructure([
+                'user',
+                'message'
+            ])
+            ->assertJsonFragment([
+                'message' => 'User successfully registered'
+            ]);
+    }
+
+    public function testRegisterRouteWithARegisteredUser()
+    {
+        $user = $this->generateANewUser();
+        $this->createANewUser($user);
+        $response = $this->createANewUser($user);
+        $response->assertStatus(400);
+    }
+
+    public function testLoginRoute()
+    {
+        $user = $this->generateANewUser();
+        $this->createANewUser($user);
+        $response = $this->login($user);
+        $response->assertSuccessful()
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'access_token',
+            ]);
+    }
+
+    public function testLoginRouteWithoutRegisteredUser()
+    {
+        $response = $this->post(route('auth.login'), [
+            'email' => $this->faker->email,
+            'password' => $this->faker->password,
+        ]);
+        $response->assertStatus(401);
+    }
+
+    public function testProfileRoute()
+    {
+        $tempUser = $this->generateANewUser();
+        $this->createANewUser($tempUser);
+        $user = User::where('email', $tempUser->email)->first();
+        $token = JWTAuth::fromUser($user);
+        $response = $this->get(route('auth.profile'), [
+            'Authorization' => "Bearer {$token}"
+        ]);
+
+        $response->assertSuccessful()
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'id',
+                'name',
+                'email',
+            ]);
+    }
+
+    public function testProfileRouteWithoutToken()
+    {
+        $tempUser = $this->generateANewUser();
+        $this->createANewUser($tempUser);
+        $user = User::where('email', $tempUser->email)->first();
+        $token = JWTAuth::fromUser($user);
+        $response = $this->get(route('auth.profile'));
+
+        $response->assertStatus(500);
+    }
+
+    public function testRefreshTokenRoute()
+    {
+        $tempUser = $this->generateANewUser();
+        $this->createANewUser($tempUser);
+        $user = User::where('email', $tempUser->email)->first();
+        $token = JWTAuth::fromUser($user);
+        $response = $this->postJson(route('auth.refresh'), [], [
+            'Authorization' => "Bearer {$token}"
+        ]);
+
+        $response->assertSuccessful()
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'access_token'
+            ]);
+    }
+
+    public function testRefreshTokenRouteWithoutRegisteredUser()
+    {
+        $response = $this->post(route('auth.refresh'), [
+            'email' => $this->faker->email,
+            'password' => $this->faker->password,
+        ]);
+        $response->assertStatus(500);
+    }
+
+    public function testLogoutRoute()
+    {
+        $tempUser = $this->generateANewUser();
+        $this->createANewUser($tempUser);
+        $user = User::where('email', $tempUser->email)->first();
+        $token = JWTAuth::fromUser($user);
+        $response = $this->postJson(route('auth.logout'), [], [
+            'Authorization' => "Bearer {$token}"
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertExactJson([
+                'message' => 'User successfully signed out'
+            ]);
+    }
+
+//    //Create user and authenticate the user
+//    protected function authenticate(){
+//        $user = User::create([
+//            'name' => 'test',
+//            'email' => 'test@gmail.com',
+//            'password' => Hash::make('secret1234'),
+//        ]);
+//        $token = JWTAuth::fromUser($user);
+//        return $token;
+//    }
+//
+
+}
